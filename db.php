8aa94d1f8510a926e3e75b6ace64f137120a6be6
sarastefa <sarastef.ok@gmail.com>
2024-11-27 00:12:03 -0300

    db.php

--- db.php
+++ db.php
@@ -1,10 +1,8 @@
 <?php
 $host = 'localhost';
 $dbname = 'sistema_usuarios';
-$username = 'root';  // Usuário padrão do MySQL
-$password = '';  // Senha padrão do MySQL
-
-// Conexão com o banco de dados MySQL
+$username = 'root'; 
+$password = ''; 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
